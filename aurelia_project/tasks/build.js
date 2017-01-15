0e0c01df3cf45379f9549824185903a506873845
feardarkness <alvarado.ariel.contact@gmail.com>
2017-01-15 14:22:54 -0400

    agregando bootstrap sass al proyecto

--- aurelia_project/tasks/build.js
+++ aurelia_project/tasks/build.js
@@ -3,6 +3,7 @@ import transpile from './transpile';
 import processMarkup from './process-markup';
 import processCSS from './process-css';
 import {build} from 'aurelia-cli';
+import debug from 'gulp-debug';
 import project from '../aurelia.json';
 
 export default gulp.series(
