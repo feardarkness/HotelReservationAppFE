8aeadcd4233520592676f516afb484c16945e0f4
Ariel Alvarado <aalvarado@agetic.gob.bo>
2017-02-01 14:34:58 -0400

    más para el blog de templetas

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 export class App {
 
   nombres;
+  evento = 'Aún no han ocurrido eventos';
 
   constructor() {
     this.message = 'Hello World!';
+  }
+
+  evento = 'Aún no han ocurrido eventos';
+  handleEvent(event) {
+    this.evento = event;
+    return true;
   }
 
 }
